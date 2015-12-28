5627ce51ce7397781e9d492be2c55c911c5767c9
Krishna Prajapati <krishna@olindata.com>
2015-12-28 19:29:35 +0530

    updating class icinga2::pnp4nagios

--- pnp4nagios.pp
+++ pnp4nagios.pp
@@ -2,6 +2,4 @@ class icinga2::pnp4nagios {
   class{'icinga2::pnp4nagios::params':} ->
   class{'icinga2::pnp4nagios::install':} ->
   class{'icinga2::pnp4nagios::service':} ->
-  Class["icinga2::pnp4nagios"]
-
 }
