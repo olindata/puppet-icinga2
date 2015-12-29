7ccef399339b6ab7c6aa46af6951ad257205a3d1
Krishna Prajapati <krishna@olindata.com>
2015-12-29 11:59:13 +0530

    pnp4nagios params file

---
+++ manifests/pnp4nagios/params.pp
@@ -0,0 +1,15 @@
+iclass icinga2::pnp4nagios::params {
+  $package_name           = 'pnp4nagios'
+  $package_ensure         = 'present'
+  $service_name           = 'npcd'
+  $service_ensure         = 'running'
+  $service_enable         = true
+  $service_manage         = true
+  $htpasswd_config        = 'pnp4nagios.conf'
+  $htpasswd_user          = 'root'
+  $htpasswd_group         = 'root'
+  $htpasswd_config_ensure = 'present'
+  $htpasswd_config_mode   = '0644'
+  $httpd_path             = '/etc/httpd/conf.d'
+}
+
