3ab7b55a12a9f9197ecf1d58081c6e55b69a490c
Krishna Prajapati <krishna@olindata.com>
2016-01-05 03:40:26 +0530

    unsupported os notification

--- manifests/pnp4nagios/params.pp
+++ manifests/pnp4nagios/params.pp
@@ -1,18 +1,26 @@
 class icinga2::pnp4nagios::params {
-  $package_name           = 'pnp4nagios'
-  $package_ensure         = 'present'
-  $service_name           = 'npcd'
-  $service_ensure         = 'running'
-  $service_enable         = true
-  $service_manage         = true
-  $htpasswd_config        = 'pnp4nagios.conf'
-  $htpasswd_user          = 'root'
-  $htpasswd_group         = 'apache'
-  $htpasswd_config_ensure = 'present'
-  $htpasswd_config_mode   = '0644'
-  $httpd_path             = '/etc/httpd/conf.d'
-  $nagios_web_user        = 'nagiosadmin'
-  $nagios_web_pass        = 'nagios'
-  $manage_php_timezone    = true
+  case $::osfamily {
+    'RedHat': {
+       $package_name           = 'pnp4nagios'
+       $package_ensure         = 'present'
+       $service_name           = 'npcd'
+       $service_ensure         = 'running'
+       $service_enable         = true
+       $service_manage         = true
+       $htpasswd_config        = 'pnp4nagios.conf'
+       $htpasswd_user          = 'root'
+       $htpasswd_group         = 'apache'
+       $htpasswd_config_ensure = 'present'
+       $htpasswd_config_mode   = '0644'
+       $httpd_path             = '/etc/httpd/conf.d'
+       $nagios_web_user        = 'nagiosadmin'
+       $nagios_web_pass        = 'nagios'
+       $manage_php_timezone    = true
+     }
+     default: {
+       fail("Module ${module_name} is not supported on ${::osfamily}")
+     }
+  }
+
 }
 
